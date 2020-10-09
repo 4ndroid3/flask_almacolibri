d11235343ca5d848263f3cea03b644713dfa156f
[4ndroid3] <[andijaimo@gmail.com]>
2020-10-09 18:31:30 -0300

    se agregan detalles visuales, se agrega products.html

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import make_response, request, redirect, render_template
-
 from app import create_app
 
 app = create_app()
@@ -11,20 +10,26 @@ def index():
 
     return response
 
-
 @app.route('/colibri', methods=['GET', 'POST'])
 def colibri():
     frstmsj = 'Alma de Colibri'
     username = 'andrés'
-    products = ['Manies', 'Nuez', 'Mix Semillas', 'Pimentón', 'Sal del Himalaya', 'Coco Rallado']
     context = {
         'frstmsj': frstmsj,
-        'username': username,
-        'products': products
+        'username': username
     }
 
     return render_template('colibri.html', **context)
+    
 @app.route('/login')
 def login():
-    
     return render_template('login.html')
+
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    products = ['Manies', 'Nuez', 'Mix Semillas', 'Pimentón', 'Sal del Himalaya', 'Coco Rallado']
+    context = {
+        'products': products
+    }
+
+    return render_template('products.html', **context)
